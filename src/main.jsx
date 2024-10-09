b238b8d8f7c2f9cc01f5c146eeb621103e6c0748
Jasurbek <jasurdev1604@gmail.com>
2024-10-09 12:23:05 +0500

    new

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Root from "./root";
+import App from "./components/app";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Root />
+    <App />
   </StrictMode>
 );
