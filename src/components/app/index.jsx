9a856d85c16b8081589897e4654e0d36be3bb0d6
Jasurbek <jasurdev1604@gmail.com>
2024-10-10 14:36:28 +0500

    tailwind css

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import styles from "./app.module.css";
 
 const App = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <div className="h-[100dvh] flex items-center justify-center ">
+      <div className="flex gap-10 max-lg:flex-col bg-slate-500 lg:bg-indigo-400">
+        <div className={styles.cart}>
+          <div className="bg-red-500 h-20 col-span-2 hover:bg-blue-500 transition">
+            col 1
+          </div>
+          <div className="bg-blue-500 h-20">col 2</div>
+        </div>
+        <div className={styles.cart}>
+          <div className="bg-red-500 h-20 col-span-2 hover:bg-blue-500 transition">
+            col 1
+          </div>
+          <div className="bg-blue-500 h-20">col 2</div>
+        </div>
+        <div className={styles.cart}>
+          <div className="bg-red-500 h-20 col-span-2 hover:bg-blue-500 transition">
+            col 1
+          </div>
+          <div className="bg-blue-500 h-20">col 2</div>
+        </div>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      transitionDuration: {
+        DEFAULT: "300ms",
+      },
+      transitionTimingFunction: {
+        DEFAULT: "ease-linear",
+      },
+    },
   },
   plugins: [],
 };
